c86c4423b93a04ee27973a718e073405bc68751b
emamo <emamoiom@gmail.com>
2016-05-02 23:44:13 +0800

    modified layout: newline and fill zero

--- myc/myc.c
+++ myc/myc.c
@@ -3,10 +3,10 @@
 int main(int argc, char* argv[]){
     signed char d;
     int i;
-    printf("show bit pattern of signed char");
+    printf("show bit pattern of signed char\n");
     for(i=0;i<=255;i++){
         d=i;
-        printf("%d: %x\n",i,d);
+        printf("%03d: %08x\n",i,d);
     }
     return 0;
 }
